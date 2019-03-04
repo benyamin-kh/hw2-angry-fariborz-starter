5f0fe4cbf6ea0fd7464094512d61042ed023108a
Hossein Rimaz <17963017+mhrimaz@users.noreply.github.com>
2019-03-04 06:07:49 +0330

    final version of starter

--- src/test/java/ir/ac/kntu/style/CheckOutputTest.java
+++ src/test/java/ir/ac/kntu/style/CheckOutputTest.java
@@ -1,21 +1,24 @@
 package ir.ac.kntu.style;
 
 import java.io.*;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.Arrays;
 import java.util.stream.Collectors;
 
 import ir.ac.kntu.AngryFariborz;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
 /**
  * 110 points
- * @author mhrimaz, sfathi
+ *
+ * @author mhrimaz
  */
 public class CheckOutputTest {
-    private final static int TIME_LIMIT = 100_000;
+    private final static int TIME_LIMIT = 1_000_000;
 
     static {
         System.err.println("$$$GRADER$$$ | { type:\"MSG\" , key:\"TOTAL\" , value:150, priority:1  }  | $$$GRADER$$$");
@@ -23,67 +26,115 @@ public class CheckOutputTest {
 
 
     private final static String SEPARATOR = "\n";
-    
 
-    private static boolean doSimulation(String inputs, String expected) {
-        InputStream inputStream = null;
-        PrintStream printStream = null;
-        try {
-            File input = new File("input.txt");
-            input.createNewFile();
-            File output = new File("output.txt");
-            output.createNewFile();
-            inputStream = new FileInputStream(input);
-            printStream = new PrintStream(output);
+    private static String runMainMethod(Method main, String inputs, String[] args)
+            throws IOException, InvocationTargetException, IllegalAccessException {
+
+        File input = new File("input.txt");
+        input.createNewFile();
+        File output = new File("output.txt");
+        output.createNewFile();
+        try (InputStream inputStream = new FileInputStream(input); PrintStream printStream = new PrintStream(output);) {
+
             try (PrintWriter w = new PrintWriter(input)) {
                 w.println(inputs);
-            } catch (Exception ex) {
-                ex.printStackTrace();
             }
 
             try (BufferedReader reader = new BufferedReader(new FileReader(output))) {
                 System.setIn(inputStream);
                 System.setOut(printStream);
-                AngryFariborz.main(new String[]{});
-                String found = reader.lines().map(line->line.trim().replaceAll("[\\r\\n\\s]+","")).sorted().collect(Collectors.joining(" "));
-                if(found == null){
-                    found = "";
-                }
-                System.err.println("found+\"\\\"\" = " + found+"\"");
-                String[] splited = expected.split("[\\r\\n\\s]+");
-                Arrays.sort(splited);
-                String expectedOutput = String.join(" ",splited);
-                System.err.println("expectedOutput = " + expectedOutput);
-                return found.equals(expectedOutput);
-            } catch (Exception ex) {
-                ex.printStackTrace();
-            }
-            return false;
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        } finally {
-            try {
-                inputStream.close();
-                printStream.close();
-            } catch (Exception ex) {
-                ex.printStackTrace();
+                main.invoke(null,(Object) args);
+                String found = reader.lines().map(line -> line.trim()
+                        .replaceAll("[\\r\\n\\s]+", ""))
+                        .sorted()
+                        .collect(Collectors.joining(" "));
+                return found;
             }
         }
-        return false;
+    }
+
+    private static boolean doSimulation(String inputs, String expected) {
+        return doSimulation(inputs,expected,false);
+    }
+
+    private static boolean doSimulation(String inputs, String expected, boolean onlyContains) {
+        Method main;
+        try {
+            main = AngryFariborz.class.getMethod("main", String[].class);
+        } catch (Exception e) {
+            return false;
+        }
+        String outputs = null;
+        try {
+            outputs = runMainMethod(main, inputs, null);
+        } catch (Exception e) {
+            System.err.println("e = " + e);
+            return false;
+        }
+
+        String[] split = expected.split("[\\r\\n\\s]+");
+        Arrays.sort(split);
+        String expectedOutput = String.join(" ", split);
+        if(onlyContains){
+            return outputs.contains(expected);
+        }else{
+            return outputs.equals(expectedOutput);
+        }
     }
 
 
-
     @Test(timeout = TIME_LIMIT)
-    public void testCase1(){
+    public void testCase1() {
         assertTrue(doSimulation("6", "NO"));
-        System.err.println("$$$GRADER$$$ | { type:\"SCORE\" , amount:10 , reason:\"Your Output is correct.\" } | $$$GRADER$$$" );
+        System.err.println("$$$GRADER$$$ | { type:\"SCORE\" , amount:5 , reason:\"The Number is Not Magical.\" } | $$$GRADER$$$");
     }
 
     @Test(timeout = TIME_LIMIT)
-    public void testCase2(){
-        assertTrue(doSimulation("94", "YES"+SEPARATOR+"(9+4)$13"+SEPARATOR+"(9-4)$5"));
-        System.err.println("$$$GRADER$$$ | { type:\"SCORE\" , amount:100 , reason:\"Your Output is correct.\" } | $$$GRADER$$$" );
+    public void testCase2() {
+        assertTrue(doSimulation("5", "YES",true));
+        System.err.println("$$$GRADER$$$ | { type:\"SCORE\" , amount:5 , reason:\"The Number is Magical.\" } | $$$GRADER$$$");
+    }
+
+    @Test(timeout = TIME_LIMIT)
+    public void testCase3() {
+        assertTrue(doSimulation("94","YES",true));
+        System.err.println("$$$GRADER$$$ | { type:\"SCORE\" , amount:10 , reason:\"The Number is Magical.\" } | $$$GRADER$$$");
+
+
+        boolean eq1 = false;
+        boolean eq2 = false;
+
+        eq1 = eq1 || doSimulation("94", "(9+4)$13" ,true);
+        eq1 = eq1 || doSimulation("94", "(4+9)$13" ,true);
+        eq1 = eq1 || doSimulation("94", "((9)+(4))$13" ,true);
+        eq1 = eq1 || doSimulation("94", "((4)+(9))$13" ,true);
+        eq1 = eq1 || doSimulation("94", "9+4$13" ,true);
+        eq1 = eq1 || doSimulation("94", "4+9$13" ,true);
+
+
+        eq2 = eq2 || doSimulation("94", "(9-4)$5" ,true);
+        eq2 = eq2 || doSimulation("94", "((9)-(4))$5" ,true);
+        eq2 = eq2 || doSimulation("94", "9-4$5" ,true);
+
+
+
+        assertTrue(eq1);
+        System.err.println("$$$GRADER$$$ | { type:\"SCORE\" , amount:30 , reason:\"First Equation is TRUE.\" } | $$$GRADER$$$");
+        assertTrue(eq2);
+        System.err.println("$$$GRADER$$$ | { type:\"SCORE\" , amount:30 , reason:\"Second Equation is TRUE.\" } | $$$GRADER$$$");
+    }
+
+    @Test(timeout = TIME_LIMIT)
+    public void testCase4() {
+        assertTrue(doSimulation("941","YES",true));
+        System.err.println("$$$GRADER$$$ | { type:\"SCORE\" , amount:10 , reason:\"The Number is Magical.\" } | $$$GRADER$$$");
+    }
+
+    @Test(timeout = TIME_LIMIT)
+    public void testCase5() {
+        assertTrue(doSimulation("9156747","YES",true));
+        System.err.println("$$$GRADER$$$ | { type:\"SCORE\" , amount:30 , reason:\"The Number is Magical.\" } | $$$GRADER$$$");
+
     }
 
 
